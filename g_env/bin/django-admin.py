5cd1623363449e1513cd8511b630fef6ffd01980
aidazh <zhoroeva.aida@gmail.com>
2020-12-20 23:11:45 +0600

    First django project

---
+++ g_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aida/Documents/bootcamp/django_projects/django_girls/g_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
